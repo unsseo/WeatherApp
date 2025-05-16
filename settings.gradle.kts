9a787fa5d3afb2a04f2af66f3535d605258b20e3
seo yeji <144655616+evamose@users.noreply.github.com>
2025-05-16 23:46:25 +0900

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,15 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // 중앙에서만 저장소 관리
     repositories {
         google()
         mavenCentral()
+        // ⬇️ 카카오 SDK 및 카카오맵 SDK 저장소 추가
+        maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
-rootProject.name = "WeatherApp"
+rootProject.name = "map_project"
 include(":app")
  
