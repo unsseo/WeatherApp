9e0a679ae216bf282402527cd02bf3b2f25d399b
김혜린 <khr29136939@gmail.com>
2025-05-22 21:33:42 +0900

    activity, 예지님 파일 추가

---
+++ app/src/main/java/com/example/basicweatherapp/HomeScreenActivity.java
@@ -0,0 +1,52 @@
+package com.example.basicweatherapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    private Button detailButton;
+    private Button regionButton;
+    private Button weeklyButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);  // 홈화면 XML
+
+        detailButton = findViewById(R.id.detail_information);
+        regionButton = findViewById(R.id.weather_region);
+        weeklyButton = findViewById(R.id.weekly_weather);
+
+        // 상세 정보 버튼 클릭 시 activity_details.xml로 이동
+        detailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreenActivity.this, DetailWindow.class);
+                startActivity(intent);
+            }
+        });
+
+        // 지역별 날씨 버튼 클릭 시 weather_map.xml로 이동
+        regionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreenActivity.this, WeatherMap.class);
+                startActivity(intent);
+            }
+        });
+
+        // 주간 날씨 버튼 클릭 시 weather_week.xml로 이동
+        weeklyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreenActivity.this, WeatherWeekActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
